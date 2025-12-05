671471eb758e4bf3108f99477a46a30d5939fdc1
YoonjinOh0909 <95potter95@gmail.com>
2025-12-05 10:54:30 +0900

    feat: 현재 만들어진 수집, 전처리, 저장 과정의 task를 dag로 합치기

--- dags/cardData_pipline.py
+++ dags/cardData_pipline.py
@@ -1,8 +1,10 @@
 from airflow.decorators import dag, task
 import pendulum
+import asyncio
 from airflow.decorators import task
 from my_utils.optimized_crawling_crawling import crawling_main
 from my_utils.upload_sql_aftercrawling import upload_main
+from my_utils.summary import process_card_summaries
 from airflow.operators.python import PythonOperator
 import json
 
@@ -10,7 +12,7 @@ import json
     dag_id='03_crawling_cardData_pipline',
     tags=['crawling', 'cardData', 'cardGorila', 'taskflow'],
     start_date=pendulum.datetime(2025, 1, 1, tz="Asia/Seoul"),
-    schedule="0 9 * * 1", # 월요일마다.
+    schedule="0 1 * * *", # 매 새벽 1시에 진행하도록
     catchup=False,
 )
 def getData_pipeline_weekly():
@@ -22,29 +24,15 @@ def getData_pipeline_weekly():
     def upload_cardData():
         upload_main()
 
-
-    # @task
-    # def get_user_id_list() -> list :
-    #     uid_list = get_all_user_id_list()
-        
-    #     return uid_list
-
-
-    # @task
-    # def update_To_DB(user_list, date_interval):
-    #     """알림 DB를 최신화 합니다."""
-    #     for user_id in user_list:
-    #         try:
-                
-    #             usage = get_seven_days_usage(user_id)
-    #             info = {"title": "주간지출", "content": f"{date_interval}에 총 {usage}원을 썼어요. {'어디에 많이 썼는지 확인하세요' if usage != '0' else ''}"}
-    #             json_string = json.dumps(info, ensure_ascii=False)
-    #             insert_val_notification(user_id, 3, json_string)
-    #         except Exception as error:
-    #             print(f"에러가 발생했습니다: {error}")
-    #             raise error
+    @task
+    def summarized_cardmaster_temp():
+        asyncio.run(process_card_summaries())
         
     crwaling = get_cardData()
     upload = upload_cardData()
-    crwaling >> upload
+    summary = summarized_cardmaster_temp()
+
+    crwaling >> upload >> summary
+
+    
 getData_pipeline_weekly()
